c5d8caa6c5ce81df54a99e760685aba56da07cf4
Jody Jacobus Geers <y2k_geers@hotmail.com>
2014-03-19 10:21:05 +0200

    Update js.js

--- build/js.js
+++ build/js.js
@@ -1,10 +1,10 @@
 /*
 Plugin Name: Small Block HTML Area
-Plugin URI: https://github.com/Jody-Geers
+Plugin URI: https://github.com/Jody-Geers/jjg-small-block-html-area
 Description: JJG HTML Block widgit.
 Version: 1.0
 Author: Jody Jacobus Geers
-Author URI: https://github.com/Jody-Geers
+Author URI: http://jody-geers.github.io/
 License: GPL3
 */
 jQuery(document).ready(function ($) {
